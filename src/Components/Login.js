0f95ac4bf5db93ad8a8da9a71aa99b9176b19256
Jessica Janei <webdevjessij@gmail.com>
2022-09-13 15:25:13 -0400

    add event for submit button

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,8 +3,14 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 function Login() {
+ 
+        function handleSubmit(e) {
+            e.preventDefault();
+            console.log ('you clicked submit!');
+        }
+
   return (
-    <Form className="allForm">
+    <Form className="allForm" onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
